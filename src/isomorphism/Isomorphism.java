752b035d73dcf84b1bac79e0e04f104957e9c014
Zosia <tomasz-korecki@wp.pl>
2013-04-07 21:19:54 +0200

    initial release

---
+++ src/isomorphism/Isomorphism.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isomorphism;
+
+/**
+ *
+ * @author Tomek
+ */
+public class Isomorphism {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
